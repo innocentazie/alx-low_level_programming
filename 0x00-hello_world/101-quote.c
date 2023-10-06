6ac210a250f94d8858e8df70596ecc1172502cb4
Innocent <azieson@gmial.com>
2023-10-06 12:32:41 -0700

    hello world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 1 (Error)
+ */
+int main(void)
+{
+	const char *message =
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	ssize_t bytes_written;
+
+	bytes_written = write(STDERR_FILENO, message, 59);
+
+	if (bytes_written == -1)
+{
+		return (1);
+}
+
+	return (1);
+}
+
